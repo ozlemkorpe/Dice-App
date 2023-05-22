fcbd3bdd2bd9b408c0fe7d94bd52851010fc2ef3
Özlem Körpe <cse.ozlemkorpe@gmail.com>
2023-05-23 00:12:38 +0300

    Button actions added

--- Dice App/ViewController.swift
+++ Dice App/ViewController.swift
@@ -8,11 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("Roll button is pressed")
+    }
 
+    @IBAction func resetButtonPressed(_ sender: UIButton) {
+        print("Reset button is pressed")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
+        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceTwo")
+        //diceImageView2.alpha = 0.5
+    // Set alpha as 0.5 in beginning.
+    // Clicking on Roll randomly selects a dice and sets alpha as 1
+    // Add reset button to reset dices to 1, it selts alpha as 0.5 again
+        
     }
+
 
 
 }
