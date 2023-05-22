287b669cf9b1ee7497e19ee1e642116c56a96300
Özlem Körpe <cse.ozlemkorpe@gmail.com>
2023-05-22 22:57:15 +0300

    Initial Commit

---
+++ Dice App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dice App
+//
+//  Created by Özlem Körpe on 22.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
